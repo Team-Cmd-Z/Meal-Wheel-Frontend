e762ec7abbbed722e49aed98e41d5de1b6b2562d
nurselaine <Elaine.t.huynh@gmail.com>
2022-07-06 15:00:03 -0700

    created spinning wheel and updated state

---
+++ src/Components/Wheel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Wheel.css';
+
+export default class Wheel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: null,
+    };
+    this.selectItem = this.selectItem.bind(this);
+  }
+
+  selectItem() {
+    if (this.state.selectedItem === null) {
+      const selectedItem = Math.floor(Math.random() * this.props.cuisines.length);
+      if (this.props.onSelectItem) {
+        this.props.onSelectItem(selectedItem);
+      }
+      this.setState({ selectedItem });
+      this.props.updateCuisine(selectedItem);
+    } else {
+      this.setState({ selectedItem: null });
+      setTimeout(this.selectItem, 500);
+    }
+  }
+
+  render() {
+    const { selectedItem } = this.state;
+    const { cuisines } = this.props;
+    console.log(this.state.selectedItem);
+    const wheelVars = {
+      '--nb-item': cuisines.length,
+      '--selected-item': selectedItem,
+    };
+    const spinning = selectedItem !== null ? 'spinning' : '';
+
+    return (
+      <div className="wheel-container">
+        <div className={`wheel ${spinning}`} style={wheelVars} onClick={this.selectItem}>
+          {cuisines.map((item, index) => (
+            <div className="wheel-item" key={index} style={{ '--item-nb': index }}>
+              {item}
+            </div>
+          ))}
+        </div>
+        <button onClick={this.selectItem}>SPIN</button>
+      </div>
+    );
+  }
+}
