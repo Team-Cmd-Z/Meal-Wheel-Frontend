97d84b92e944543994ccf9f6eff38a3aae29869f
nurselaine <Elaine.t.huynh@gmail.com>
2022-07-07 16:17:01 -0700

    removed homepage recipe card add notes feature

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -3,9 +3,9 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import UpdateRecipe from './UpdateRecipe';
 
-class RecipeCard extends React.Component{
-  render(){
-    return(
+class RecipeCard extends React.Component {
+  render() {
+    return (
       <>
         <Card style={{ width: '18rem' }}>
           <Card.Img variant='top' src={this.props.obj.image || this.props.obj.imageUrl} />
@@ -13,14 +13,18 @@ class RecipeCard extends React.Component{
             <Card.Title>{this.props.obj.title}</Card.Title>
             <Button onClick={() => this.props.handleOnShowModal(this.props.obj)}>See Recipe</Button>
             {this.props.saved ? <Button onClick={() => this.props.onDelete(this.props.obj._id)} variant='danger'>Delete</Button> : ''}
-            <UpdateRecipe
-              updateRecipe = {this.props.updateRecipe}
-              obj={this.props.obj}
-              getSavedRecipes = {this.props.getSavedRecipes}
-            />
-            <Card.Text>
-              Notes: {this.props.obj.notes}
-            </Card.Text>
+            {this.props.saved &&
+              <>
+                <UpdateRecipe
+                  updateRecipe={this.props.updateRecipe}
+                  obj={this.props.obj}
+                  getSavedRecipes={this.props.getSavedRecipes}
+                />
+                <Card.Text>
+                  Notes: {this.props.obj.notes}
+                </Card.Text>
+              </>
+            }
           </Card.Body>
         </Card>
       </>
