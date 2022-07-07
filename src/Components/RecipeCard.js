047245d0e59c8a94533860e2870bf54f3abf48e3
nurselaine <Elaine.t.huynh@gmail.com>
2022-07-07 01:09:42 -0700

    created delete and put routes for saved recipe page and fixed home page bug

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -7,10 +7,11 @@ class RecipeCard extends React.Component{
     return(
       <>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant='top' src={this.props.obj.image} />
+          <Card.Img variant='top' src={this.props.obj.image || this.props.obj.imageUrl} />
           <Card.Body>
             <Card.Title>{this.props.obj.title}</Card.Title>
             <Button onClick={() => this.props.handleOnShowModal(this.props.obj)}>See Recipe</Button>
+            {this.props.saved ? <Button onClick={() => this.props.onDelete(this.props.obj._id)} variant='danger'>Delete</Button> : ''}
           </Card.Body>
         </Card>
       </>
